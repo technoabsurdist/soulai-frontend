ac177f49dec07d509dc2b1889e6392718e3cce59
techno absurdist <92827957+technoabsurdist@users.noreply.github.com>
2023-10-18 17:08:10 -0500

    all

---
+++ src/componentes/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="container mx-auto mt-auto lg:w-2/3 p-5 text-justify lg:text-left text-sm text-zinc-500 ">
+    </footer>
+  );
+};
+
+export default Footer;
